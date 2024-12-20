65ffd8dcf1e92a75ce084204cf800e97d40937b6
homosapian <ankitd349@gmail.com>
2024-12-20 16:57:34 +0530

    Add state for showing description

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export interface Job {
   id: string;
   title: string;
@@ -14,6 +16,10 @@ export interface Job {
 }
 
 const JobListing = ({ job }: { job: Job }) => {
+  const [showDescription, setShowDescription] = useState(false);
+  let description = job.description;
+  if (!showDescription) description = description.slice(0, 90) + "...";
+
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -21,8 +27,14 @@ const JobListing = ({ job }: { job: Job }) => {
           <div className="text-gray-600 my-2">{job.type}</div>
           <h3 className="text-xl font-bold">{job.title}</h3>
         </div>
-        <div className="mb-5">{job.description}</div>
+        <div className="mb-5">{description}</div>
         <h3 className="text-indigo-500 mb-2">${job.salary}</h3>
+        <button
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+          onClick={() => setShowDescription((prev) => !prev)}
+        >
+          {showDescription ? "Show Less" : "Show More"}
+        </button>
         <div className="border border-gray-100 mb-5"></div>
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
