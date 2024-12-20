15208876a33aa9541b4d88800206e6f3018bed49
homosapian <ankitd349@gmail.com>
2024-12-20 11:37:44 +0530

    Add tailwind to the project and basic jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const name = 'John';
+  const names = ['John', 'Doe', 'Jane'];
+  const loggedIn = true;
+  const styles = {
+    color:'red',
+    fontSize:'24px'
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='text-5xl'>App</div>
+      <p style={styles}>Hello {name}</p>
+        <ul>
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
+      {loggedIn && <p>Welcome back {name}</p>} 
     </>
   )
 }
